f54dbaf71acf16c4045d1ce84ce1ef7a6eab4819
mrworld196wide <shubhum19@gmail.com>
2023-02-19 09:15:09 +0530

    413. Campground model basics 19-2-2023 9:14am

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 const express = require('express');
-const app= express();
 const path = require('path');
+const mongoose = require('mongoose');
+const campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    // useCreateIndex: true,   //don't use this as it has been deprecated from mongoose
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+
 // adding  ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 app.get('/', (req, res) => {
     res.render('home')
 });
+
+app.get('/makeCampground', async(req, res) =>{
+    const camp=  new campground({title:'ghar ka angan', description:'free of cost'});
+    await camp.save;
+    res.send(camp)
+})
 
 
 app.listen(3000, () => {
