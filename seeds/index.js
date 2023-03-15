0dd7a797d730bf0881e82152b99ab87146545bd2
mrworld196wide <shubhum19@gmail.com>
2023-03-16 00:27:09 +0530

    432. Adding Images 16-02-23 12:26am

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa ut quibusdam necessitatibus.',
+            price
         })
         await camp.save();
     }
