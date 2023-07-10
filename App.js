b85577dfcdd560fbe5f45b7601f3c8e9e97ee44e
mrworld196wide <shubhum19@gmail.com>
2023-07-10 18:42:40 +0530

    addd JOI to the project

--- App.js
+++ App.js
@@ -3,22 +3,25 @@ const path = require('path');
 const mongoose = require('mongoose');
 // adding ejs mate
 const ejsMate = require('ejs-mate');
-//methodOverride middleware allows us to use HTTP verbs such as PUT or DELETE in HTML forms
-const methodOverride = require("method-override");
-const Campground = require('./models/campground');
+// importing JOI part
+const { campgroundSchema } = require('./schemas.js');
 // importing utilities function
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+//methodOverride middleware allows us to use HTTP verbs such as PUT or DELETE in HTML forms
+const methodOverride = require("method-override");
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
-    // useCreateIndex: true,   //don't use this as it has been deprecated from mongoose
+    useCreateIndex: true,   //don't use this in newer versions as it has been deprecated from mongoose
     useUnifiedTopology: true
 });
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+    console.log("Database connected successfully!");
 });
 
 const app = express();
@@ -38,6 +41,17 @@ app.use(express.urlencoded({ extended: true }));
 // will be used to override the HTTP method. like DELELTE and PUT
 app.use(methodOverride('_method'));
 
+// JOI part to validate the camprgrounds
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
 // home.ejs
 app.get('/', (req, res) => {
     res.render('home')
@@ -46,22 +60,23 @@ app.get('/', (req, res) => {
 // index.ejs
 app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
+    res.render('campgrounds/index', { campgrounds })
 }));
 
 // new.ejs
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+// for posting 
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-}));
+}))
 
 // show.ejs
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res,) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
 }));
@@ -70,12 +85,11 @@ app.get('/campgrounds/:id', catchAsync(async (req, res) => {
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
-}));
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
-    // res.send("It worked!");
+}))
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`);
+    res.redirect(`/campgrounds/${campground._id}`)
 }));
 
 // Deleting a campground
@@ -89,16 +103,16 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
 // methods (GET, POST, PUT, DELETE, etc.) for a given route.
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
-});
+})
 
+//default error handler with error template
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
-});
+})
 
 // configuring the server
 app.listen(3000, () => {
     console.log('Serving on port 3000')
-});
-
+})
