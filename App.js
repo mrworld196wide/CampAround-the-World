fb4646b28e3fd3759b825c33f5fccabc9b6a3a05
mrworld196wide <shubhum19@gmail.com>
2023-07-13 00:46:45 +0530

    Delete route added

--- App.js
+++ App.js
@@ -121,7 +121,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-
+// deleting review
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
 
 
 // app.all() method, which is used to handle all HTTP 
