1ce876f2da5f0695a5df60235635bd80eecc1a89
mrworld196wide <shubhum19@gmail.com>
2023-07-20 11:50:14 +0530

    adding map show page

--- seeds/index.js
+++ seeds/index.js
@@ -28,6 +28,10 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/dbp6pwjvu/image/upload/v1689772146/YelpCamp/eurvomrw1wwgrqbrabbf.jpg',
